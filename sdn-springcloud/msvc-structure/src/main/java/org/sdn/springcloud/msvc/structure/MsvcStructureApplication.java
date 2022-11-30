d600cb61cae45449ea4e5e9ca946eca6813687f5
Nahum Martinez <nmartinez.salgado@yahoo.com>
2022-11-29 21:06:16 -0600

    NAM

---
+++ sdn-springcloud/msvc-structure/src/main/java/org/sdn/springcloud/msvc/structure/MsvcStructureApplication.java
@@ -0,0 +1,13 @@
+package org.sdn.springcloud.msvc.structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcStructureApplication.class, args);
+	}
+
+}
---
+++ sdn-springcloud/msvc-structure/src/test/java/org/sdn/springcloud/msvc/structure/MsvcStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sdn.springcloud.msvc.structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
